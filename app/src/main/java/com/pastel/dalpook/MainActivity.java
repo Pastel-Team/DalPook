da6bd0b549509ebe1adfe5292c9671f69f2f0d8d
Pastel-Team <pastel.team.official@gmail.com>
2020-10-20 17:18:08 +0900

    커밋 테스트

--- app/src/main/java/com/pastel/dalpook/MainActivity.java
+++ app/src/main/java/com/pastel/dalpook/MainActivity.java
@@ -33,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                // 대충 수정했다는 의미
             }
         });
     }
