691a08201dfb760aeceb37af2591d80b27919627
Pastel-Team <pastel.team.official@gmail.com>
2020-10-22 19:45:07 +0900

    프래그먼트 설계

---
+++ app/src/main/java/com/pastel/dalpook/Calendar/CalMonthActivity.java
@@ -0,0 +1,16 @@
+package com.pastel.dalpook.Calendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pastel.dalpook.R;
+
+public class CalMonthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cal_month);
+    }
+}
--- app/src/main/java/com/pastel/dalpook/Fragment/Fragment_Cal.java
+++ app/src/main/java/com/pastel/dalpook/Fragment/Fragment_Cal.java
@@ -1,14 +1,18 @@
 package com.pastel.dalpook.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.pastel.dalpook.Calendar.CalMonthActivity;
 import com.pastel.dalpook.R;
 
 import java.text.SimpleDateFormat;
@@ -19,6 +23,9 @@ import java.util.Objects;
 
 public class Fragment_Cal extends Fragment {
 
+    private RecyclerView rcv;
+    private Button btn_test;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -26,11 +33,19 @@ public class Fragment_Cal extends Fragment {
         View rootView = lf.inflate(R.layout.fragment_second, container, false); //pass the correct layout name for the fragment
         init(rootView);
 
+        btn_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), CalMonthActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return rootView;
     }
 
     private void init(View view){
-
+        rcv = (RecyclerView)view.findViewById(R.id.rcv_cal);
+        btn_test = (Button)view.findViewById(R.id.btn_test);
     }
 }
