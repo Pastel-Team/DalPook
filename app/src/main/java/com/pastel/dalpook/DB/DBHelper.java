872788f0a69053905cea99f0bd40cfe42a389618
Pastel-Team <pastel.team.official@gmail.com>
2020-10-25 19:17:38 +0900

    10/25 설정 DB 구현

---
+++ app/src/main/java/com/pastel/dalpook/DB/DBHelper.java
@@ -0,0 +1,150 @@
+package com.pastel.dalpook.DB;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME = "dalpook.db";
+    private static final int DATABASE_VERSION = 3;
+
+    // 설정 테이블 T:true (on) / F:false (off)
+    public static final String TABLE_NAME_SET = "SETTINGS";
+    public static final String COLUMN_PUSH = "push";
+    public static final String COLUMN_MONTH = "month";
+    public static final String COLUMN_MONTH_LIST = "month_list";
+    public static final String COLUMN_WEEK = "week";
+    public static final String COLUMN_LESSON = "lesson";
+    public static final String COLUMN_WORK = "work";
+    public static final String COLUMN_DIARY = "diary";
+
+    // 기록 내용
+    public static final String TABLE_NAME_CONT = "CONTENTS";
+    public static final String COLUMN_DATE = "date";
+    public static final String COLUMN_TIME = "time";
+    public static final String COLUMN_CONT = "cont";
+    public static final String COLUMN_FLAG = "flag"; //달력 식별용.  M:월간 / W:주간 / L:수강표 / B:업무일지 / D:다이어리
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        // 설정 테이블
+        db.execSQL(" CREATE TABLE " + TABLE_NAME_SET + " (" +
+                COLUMN_PUSH + " TEXT, " +
+                COLUMN_MONTH + " TEXT," +
+                COLUMN_MONTH_LIST + " TEXT," +
+                COLUMN_WEEK + " TEXT," +
+                COLUMN_LESSON + " TEXT," +
+                COLUMN_WORK + " TEXT," +
+                COLUMN_DIARY + " TEXT);"
+        );
+
+        // 기록 테이블
+        db.execSQL(" CREATE TABLE " + TABLE_NAME_CONT + " (" +
+                COLUMN_DATE + " TEXT, " +
+                COLUMN_TIME + " TEXT," +
+                COLUMN_CONT + " TEXT, " +
+                COLUMN_FLAG + " TEXT);"
+        );
+
+        // 초기 설정
+        ContentValues infValues = new ContentValues();
+        infValues.put(COLUMN_PUSH, "T"); // 초기 티어 돌맹이
+        infValues.put(COLUMN_MONTH, "T");
+        infValues.put(COLUMN_MONTH_LIST, "T");
+        infValues.put(COLUMN_WEEK, "T");
+        infValues.put(COLUMN_LESSON, "T");
+        infValues.put(COLUMN_WORK, "T");
+        infValues.put(COLUMN_DIARY, "T");
+        db.insert(TABLE_NAME_SET, null, infValues);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        // you can implement here migration process
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_SET);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME_CONT);
+        this.onCreate(db);
+    }
+
+    /**
+     * Query only 1 record
+     **/
+    // INF 데이터 셀렉트
+    public Cursor getSets() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT  * FROM " + TABLE_NAME_SET;
+        Cursor cursor = db.rawQuery(query, null);
+
+        return cursor;
+    }
+
+    /**
+     * Query only 1 record
+     **/
+    // GOAL 데이터 셀렉트
+    public Cursor getConts() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "SELECT  * FROM " + TABLE_NAME_CONT;
+        Cursor cursor = db.rawQuery(query, null);
+
+        return cursor;
+    }
+
+
+    // 세팅값 변경
+    public void updateSets(String content, String flag) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        if(content.equals("push")){
+            db.execSQL("UPDATE " + TABLE_NAME_SET + " SET '" + COLUMN_PUSH + "' = '" + flag + "'");
+        }else if(content.equals("month")){
+            db.execSQL("UPDATE " + TABLE_NAME_SET + " SET '" + COLUMN_MONTH + "' = '" + flag + "'");
+        }else if(content.equals("month_list")){
+            db.execSQL("UPDATE " + TABLE_NAME_SET + " SET '" + COLUMN_MONTH_LIST + "' = '" + flag + "'");
+        }else if(content.equals("week")){
+            db.execSQL("UPDATE " + TABLE_NAME_SET + " SET '" + COLUMN_WEEK + "' = '" + flag + "'");
+        }else if(content.equals("lesson")){
+            db.execSQL("UPDATE " + TABLE_NAME_SET + " SET '" + COLUMN_LESSON + "' = '" + flag + "'");
+        }else if(content.equals("work")){
+            db.execSQL("UPDATE " + TABLE_NAME_SET + " SET '" + COLUMN_WORK + "' = '" + flag + "'");
+        }else if(content.equals("diary")){
+            db.execSQL("UPDATE " + TABLE_NAME_SET + " SET '" + COLUMN_DIARY + "' = '" + flag + "'");
+        }
+        //Toast.makeText(context, "수정되었습니다.", Toast.LENGTH_SHORT).show();
+    }
+
+    // 기록 내용 변경
+    public void updateConts(String date, String time, String content, String flag) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("UPDATE " + TABLE_NAME_CONT + " SET '" + COLUMN_DATE + "' = '" + date + "', '" + COLUMN_TIME + "' = '"+ time +"', '" + COLUMN_CONT + "', '" + content + "'" +
+                "WHERE '" + COLUMN_DATE + "' = '" + date + "' AND '" + COLUMN_TIME + "' = '" + time + "' AND '" + COLUMN_FLAG + "' = '" + flag + "'");
+    }
+
+    // 기록 내용 추가
+    public void insertConts(String date, String time, String content, String flag){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues infValues = new ContentValues();
+        infValues.put(COLUMN_DATE, date);
+        infValues.put(COLUMN_TIME, time);
+        infValues.put(COLUMN_CONT, content);
+        infValues.put(COLUMN_FLAG, flag);
+        db.insert(TABLE_NAME_CONT, null, infValues);
+    }
+
+    // 기록 삭제
+    public void deleteConts(String date, String time, String flag) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DELETE FROM " + TABLE_NAME_CONT + " WHERE '" + COLUMN_DATE + "' = '" + date + "' AND '" + COLUMN_TIME + "' = '" + time + "' AND '" + COLUMN_FLAG + "' = '" + flag + "'" );
+        //Toast.makeText(context, "삭제되었습니다.", Toast.LENGTH_SHORT).show();
+
+    }
+}
--- app/src/main/java/com/pastel/dalpook/Fragment/Fragment_Setting.java
+++ app/src/main/java/com/pastel/dalpook/Fragment/Fragment_Setting.java
@@ -1,32 +1,198 @@
 package com.pastel.dalpook.Fragment;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.pastel.dalpook.DB.DBHelper;
+import com.pastel.dalpook.DB.DBModels;
 import com.pastel.dalpook.R;
 import com.pastel.dalpook.ViewPagerAdapter;
 
 import java.util.Objects;
 
+import kotlinx.coroutines.channels.ChannelKt;
+
 public class Fragment_Setting extends Fragment {
+
+    private DBHelper dbHelper;
+    private DBModels dbModels;
+
+    private CheckBox chk_push;
+    private CheckBox chk_month;
+    private CheckBox chk_month_list;
+    private CheckBox chk_week;
+    private CheckBox chk_lesson;
+    private CheckBox chk_work;
+    private CheckBox chk_diary;
+    private Button btn_donate;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         LayoutInflater lf = Objects.requireNonNull(getActivity()).getLayoutInflater();
         View rootView = lf.inflate(R.layout.fragment_third, container, false); //pass the correct layout name for the fragment
-        init(rootView);
 
+        init(rootView);
+        getDB();
+
+        if(dbModels.getPush().equals("T")){
+            chk_push.setChecked(true);
+            chk_push.setText("ON");
+        }else{
+            chk_push.setChecked(false);
+            chk_push.setText("OFF");
+        }
+        if(dbModels.getMonth().equals("T")){
+            chk_month.setChecked(true);
+        }else{
+            chk_month.setChecked(false);
+        }
+        if(dbModels.getMonth_list().equals("T")){
+            chk_month_list.setChecked(true);
+        }else{
+            chk_month_list.setChecked(false);
+        }
+        if(dbModels.getWeek().equals("T")){
+            chk_week.setChecked(true);
+        }else{
+            chk_week.setChecked(false);
+        }
+        if(dbModels.getLesson().equals("T")){
+            chk_lesson.setChecked(true);
+        }else{
+            chk_lesson.setChecked(false);
+        }
+        if(dbModels.getWork().equals("T")){
+            chk_work.setChecked(true);
+        }else{
+            chk_work.setChecked(false);
+        }
+        if(dbModels.getDiary().equals("T")){
+            chk_diary.setChecked(true);
+        }else{
+            chk_diary.setChecked(false);
+        }
+
+        chk_push.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(chk_push.isChecked()){
+                    chk_push.setText("ON");
+                    dbHelper.updateSets("push", "T");
+                }else{
+                    chk_push.setText("OFF");
+                    dbHelper.updateSets("push", "F");
+                }
+            }
+        });
+
+        chk_month.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(chk_month.isChecked()){
+                    dbHelper.updateSets("month", "T");
+                }else{
+                    dbHelper.updateSets("month", "F");
+                }
+            }
+        });
+
+        chk_month_list.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(chk_month_list.isChecked()){
+                    dbHelper.updateSets("month_list", "T");
+                }else{
+                    dbHelper.updateSets("month_list", "F");
+                }
+            }
+        });
+
+        chk_week.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(chk_week.isChecked()){
+                    dbHelper.updateSets("week", "T");
+                }else{
+                    dbHelper.updateSets("week", "F");
+                }
+            }
+        });
+
+        chk_lesson.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(chk_lesson.isChecked()){
+                    dbHelper.updateSets("lesson", "T");
+                }else{
+                    dbHelper.updateSets("lesson", "F");
+                }
+            }
+        });
+
+        chk_work.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(chk_work.isChecked()){
+                    dbHelper.updateSets("work", "T");
+                }else{
+                    dbHelper.updateSets("work", "F");
+                }
+            }
+        });
+
+        chk_diary.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(chk_diary.isChecked()){
+                    dbHelper.updateSets("diary", "T");
+                }else{
+                    dbHelper.updateSets("diary", "F");
+                }
+            }
+        });
 
         return rootView;
     }
 
     private void init(View view){
+        chk_push = (CheckBox) view.findViewById(R.id.chk_push);
+        chk_month = (CheckBox) view.findViewById(R.id.chk_month);
+        chk_month_list = (CheckBox) view.findViewById(R.id.chk_month_list);
+        chk_week = (CheckBox) view.findViewById(R.id.chk_week);
+        chk_lesson = (CheckBox) view.findViewById(R.id.chk_lesson);
+        chk_work = (CheckBox) view.findViewById(R.id.chk_work);
+        chk_diary = (CheckBox) view.findViewById(R.id.chk_diary);
+        btn_donate = (Button) view.findViewById(R.id.btn_donate);
+    }
+
+    private void getDB(){
+        dbModels = new DBModels();
+        dbHelper = new DBHelper(getContext());
+        Cursor cursorSets = dbHelper.getSets();
+
+        //세팅 정보
+        if (cursorSets.getCount() > 0) {
+            cursorSets.moveToFirst();
+
+            dbModels.setPush(cursorSets.getString(0));
+            dbModels.setMonth(cursorSets.getString(1));
+            dbModels.setMonth_list(cursorSets.getString(2));
+            dbModels.setWeek(cursorSets.getString(3));
+            dbModels.setLesson(cursorSets.getString(4));
+            dbModels.setWork(cursorSets.getString(5));
+            dbModels.setDiary(cursorSets.getString(6));
+        }
 
     }
 }
